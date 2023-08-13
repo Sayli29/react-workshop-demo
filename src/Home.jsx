aa6f25580e82c226fe3ac4c510350e8e31eef3c2
Sayli29 <sayli.tawhare@gmail.com>
2023-08-13 18:31:13 +0530

    feat: added homepage content

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,19 @@
+import {useState} from 'react';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title:'blog1', body:'lorem ipsum....', author:'tom', id:1},
+        {title:'blog2', body:'lorem ipsum....', author:'Rom', id:2},
+        {title:'blog3', body:'lorem ipsum....', author:'jerry', id:3}])
     return(
-        <h2>Homepage</h2>
+        <div className='home'>
+            {blogs.map(blog=>(
+                <div className='blog-preview' key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
     );
 }
 
